2b391fc98d09a78d81082def14de7528968ccf2e
dromado <dromado>
2018-08-08 19:50:49 +0200

    init repo

---
+++ src/app/app.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+import { MAT_CHECKBOX_CLICK_ACTION } from '@angular/material/checkbox';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'app';
+}
